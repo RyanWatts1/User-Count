69128667d118a1d623d4c0fa9b3c4bbc8495854e
RyanWatts1 <devshop202@yahoo.com>
2023-01-21 19:45:35 -0500

    payload

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,27 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  return state;
-};
+// const counterReducer = (state = { counter: 0 }, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//     };
+//   }
 
-const store = createStore(counterReducer);
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//     };
+//   }
 
-export default store;
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+
+// export default store;
